a54e2dcb650d1dda5794897ea71f25cd642407e0
XCL <XCL@XCL-PC>
2017-03-17 20:27:40 +0800

    医学数据挖掘 数据预处理 马氏距离检测异常值

---
+++ outliers/FindOutliersUsingMahalanobisDistance.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#马氏距离检查异常值
+import pandas as pd
+import numpy as np
+from numpy import float64
+from matplotlib import pyplot as plt
+from scipy.spatial import distance
+from pandas import Series
+#身高数据
+Height_cm = np.array([164, 167, 168, 168, 169, 169, 169, 170, 172, 173, 175, 176, 178], dtype=float64)
+#体重数据
+Weight_kg = np.array([55,  57,  58,  56,  57,  61,  61,  61,  64,  62,  56,  66,  70], dtype=float64)
+#年龄数据
+Age = np.array([13,  12,  14,  17,  15,  14,  16,  16,  13,  15,  16,  14,  16], dtype=float64)
+hw = {'Height_cm': Height_cm, 'Weight_kg': Weight_kg, 'Age': Age}#hw为矩阵 三列三个变量（身高、体重、年龄） 行为变量序号
+hw = pd.DataFrame(hw)
+
+n_outliers = 2
+
+m_dist_order =  Series([float(distance.mahalanobis(hw.iloc[i], hw.mean(), np.mat(hw.cov().as_matrix()).I) ** 2)
+       for i in range(len(hw))]).sort_values(ascending=False).index.tolist()
+is_outlier = [False, ] * 13
+for i in range(n_outliers):
+    is_outlier[m_dist_order[i]] = True
+# print is_outlier
+
+color = ['g', 'r']
+pch = [1 if is_outlier[i] == True else 0 for i in range(len(is_outlier))]
+cValue = [color[is_outlier[i]] for i in range(len(is_outlier))]
+# print cValue
+
+fig = plt.figure()
+ax1 = plt.subplot(111, projection='3d')
+ax1.set_title('Scatter Plot')
+ax1.set_xlabel('Height_cm')
+ax1.set_ylabel('Weight_kg')
+ax1.set_zlabel('Age')
+ax1.scatter(hw['Height_cm'], hw['Weight_kg'], hw['Age'],  s=40, c=cValue)
+plt.show()
---
+++ outliers/FindOutliersUsingZScore.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+from numpy import float64
+#身高数据
+Height_cm = np.array([164, 167, 168, 169, 169, 170, 170, 170, 171, 172, 172, 173, 173, 175, 176, 178], dtype=float64)
+#体重数据
+Weight_kg = np.array([54,  57,  58,  60,  61,  60,  61,  62,  62,  64,  62,  62,  64,  56,  66,  70], dtype=float64)
+hw = {'Height_cm': Height_cm, 'Weight_kg': Weight_kg}
+hw = pd.DataFrame(hw)#hw为矩阵 两列两个变量（身高和体重） 行为变量序号
+print hw
+
+
+from sklearn import preprocessing
+from matplotlib import pyplot as plt
+#scale 数据进行标准化    公式为：(X-mean)/std  将数据按按列减去其均值，并处以其方差   结果是所有数据都聚集在0附近，方差为1。
+is_height_outlier = abs(preprocessing.scale(hw['Height_cm'])) > 2 #线性归一化    数据中心的标准偏差与2比较
+is_weight_outlier = abs(preprocessing.scale(hw['Weight_kg'])) > 2
+is_outlier = is_height_outlier | is_weight_outlier#按位或 表示两个变量中有一位是异常的 本组数据（体重，身高）异常    is_outlier是数组，值为True或False
+color = ['g', 'r']
+pch = [1 if is_outlier[i] == True else 0 for i in range(len(is_outlier))]#pch是数组，值为1或0,1表示异常点
+cValue = [color[is_outlier[i]] for i in range(len(is_outlier))]#颜色数组
+# print is_height_outlier
+# print cValue
+fig = plt.figure()
+plt.title('Scatter Plot')
+plt.xlabel('Height_cm')
+plt.ylabel('Weight_kg')
+plt.scatter(hw['Height_cm'], hw['Weight_kg'], s=40, c=cValue)#散点图 s代表图上点大小
+plt.show()
